852ae36517eca6d3baa8438f3b78319b60fff855
puteri husnul k <husnulputeri@gmail.com>
2021-09-19 16:45:12 +0700

    Latihan Var, let & Const

---
+++ QuickTip #5 Var, Let, Const/script.js
@@ -0,0 +1,23 @@
+// hoisting untuk variabel
+// var i;
+// console.log(i);
+// i = 10;
+// ---
+// function tes() {
+//     for( var i = 0; i < 10; i++ ) {
+//         console.log(i);
+//     }
+// }
+// tes();
+// ----
+// for( let i = 0 ; i < 10; i++ ) {
+//         console.log(i);
+//     }
+
+// console.log(i);
+// ----
+const i = [1,2,3]
+
+i.push(4);
+
+console.log(i);
