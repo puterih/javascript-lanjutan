267f302f4399103d78cf6f7826f4498526a612f6
puteri husnul k <husnulputeri@gmail.com>
2021-09-25 23:05:42 +0700

    Latihan Prototype

--- Object/script.js
+++ Object/script.js
@@ -31,38 +31,132 @@
 
 //    2. Function Declaration
 
-const methodMahasiswa = {
-    // method
-    makan: function(porsi) {
-        this.energy += porsi;
-        console.log(`Halo ${this.nama}, selamat makan!`);
-    },
-    // jika ditambah method
-    main: function(jam) {
-        this.energy -= jam;
-        console.log(`Halo ${this.nama}, selamat bermain!`);
-    },
-// membuat method baru di dalam object
-    tidur: function (jam) {
-        this.energy += jam * 2;
-        console.log(`Halo ${this.nama}, selamat tidur!`);
-    }
-};
+// const methodMahasiswa = {
+//     // method
+//     makan: function(porsi) {
+//         this.energy += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan!`);
+//     },
+//     // jika ditambah method
+//     main: function(jam) {
+//         this.energy -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain!`);
+//     },
+// // membuat method baru di dalam object
+//     tidur: function (jam) {
+//         this.energy += jam * 2;
+//         console.log(`Halo ${this.nama}, selamat tidur!`);
+//     }
+// };
 
-function Mahasiswa(nama, energy) {
-// bagaimana function declaration ini tau bahwa 3 method(main,makan,tidur) yg dibuat di object lain ini punya object mahasiswa??
-// caranya menggunakan Object.create() dengan menggunakan ini bisa langsung kasih tau parent object yg terhubung dgn Mahasiswa, jd tidak perlu daftar ulang methodnya
-    let mahasiswa = Object.create(methodMahasiswa);
-// buat property: mahasiswa.nama = kemudian isi apapun di dalam parameter: nama;
-    mahasiswa.nama = nama;
-    mahasiswa.energy = energy;
+// function Mahasiswa(nama, energy) {
+// // bagaimana function declaration ini tau bahwa 3 method(main,makan,tidur) yg dibuat di object lain ini punya object mahasiswa??
+// // caranya menggunakan Object.create() dengan menggunakan ini bisa langsung kasih tau parent object yg terhubung dgn Mahasiswa, jd tidak perlu daftar ulang methodnya
+//     let mahasiswa = Object.create(methodMahasiswa);
+// // buat property: mahasiswa.nama = kemudian isi apapun di dalam parameter: nama;
+//     mahasiswa.nama = nama;
+//     mahasiswa.energy = energy;
 
-// Harus return
-   return mahasiswa;
-}
-// instansiasi untuk menambah siswa
-let puteri = Mahasiswa('Puteri Husnul', 10);
-let Khotimah = Mahasiswa('Khotimah', 20);
+// // Harus return
+//    return mahasiswa;
+// }
+// // instansiasi untuk menambah siswa
+// let puteri = Mahasiswa('Puteri Husnul', 10);
+// let Khotimah = Mahasiswa('Khotimah', 20);
+
+
+
+
+// constructor function
+// function Mahasiswa(nama, energy) {
+//     // let this = Object.create(mahasiswa.prototype);
+// // object.create seperti memanggil class yang lain untuk dijadikan sebagai class parent nya 
+//     this.nama = nama;
+//     this.energy = energy;
+// }
+// Mahasiswa.prototype.makan = function(porsi) {
+//     this.energy += porsi;
+//     return `Halo ${this.nama}, selamat makan!`;
+// }
+
+// Mahasiswa.prototype.main = function (jam) {
+//     this.energy -= jam;
+//     return `Halo ${this.nama}, selamat bermain!`;
+// }
+
+// Mahasiswa.prototype.tidur = function (jam) {
+//     this.energy += jam * 2;
+//     return `Halo ${this.nama}, selamat tidur!`;
+// }
+
+// let puteri = new Mahasiswa('Puteri', 10);
+
+
+// ---
+// versi class
+// class Mahasiswa {
+//     constructor(nama, energy) {
+//         this.nama = nama;
+//         this.energy = energy;
+//     }
+// // bikin method
+
+
+// makan (porsi) {
+//     this.energy += porsi;
+//     return `Halo ${this.nama}, selamat makan!`;
+// }
+
+// main (jam) {
+//     this.energy -= jam;
+//     return `Halo ${this.nama}, selamat bermain!`;
+// }
+
+// tidur (jam) {
+//     this.energy += jam * 2;
+//     return `Halo ${this.nama}, selamat tidur!`;
+// }
+// }
+
+// let puteri = new Mahasiswa('Puteri', 10);
+// let husnul = new Mahasiswa('Husnul', 20);
+
+
+
+let angka = [3, 1, 2];
+// console.log(angka.reverse());
+// reverse untuk balikkan isi array
+// console.log(angka.sort());
+// sort untuk mengurutkan
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -95,5 +189,5 @@ let Khotimah = Mahasiswa('Khotimah', 20);
 
 
 
-//    4. Object.create()
-// ---
+//    4. Object.create() 
+
