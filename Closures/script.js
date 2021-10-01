03d8dc2d41abc94ca3f675474d9afb4b46064623
puteri husnul k <husnulputeri@gmail.com>
2021-10-01 23:38:27 +0700

    Latihan Closures

---
+++ Closures/script.js
@@ -0,0 +1,62 @@
+// Closure merupakan kombinasi antara function dan lingkungan leksikal(lexical scope) di dalam function tersebut.
+// Closure adalah sebuah function ketika memiliki akses ke parent scope-nya, meskipun parent scope-nya sudah selesai dieksekusi
+// --
+// function init() {
+//     // let nama = 'Puteri';
+//     return function (nama) {
+//         console.log(nama);
+//     }
+
+// }
+// let panggilNama = init();
+// panggilNama('Puteri');
+// panggilNama('Husnul');
+// --
+// Closure untuk membuat Function Factories, privat method
+// ---
+
+function ucapkanSalam(waktu) {
+    return function(nama) {
+        console.log(`Halo ${nama}, Selamat ${waktu}, semoga harimu menyenangkan`);
+    }
+}
+
+let selamatPagi = ucapkanSalam('Pagi');
+let selamatSiang = ucapkanSalam('Siang');
+let selamatMalam = ucapkanSalam('Malam');
+
+console.dir(selamatMalam('Puteri'));
+// ---
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
