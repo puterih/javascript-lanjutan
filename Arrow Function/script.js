4a74919756266280c2fe2d06abf3fd94a4273e9f
puteri husnul k <husnulputeri@gmail.com>
2021-10-02 22:25:10 +0700

    Latihan Arrow Function

---
+++ Arrow Function/script.js
@@ -0,0 +1,22 @@
+// FUNCTION EXPRESSION
+// const tampilNamna = function(nama) {
+//     return `Halo, ${nama}`;
+// }
+// console.log(tampilNamna('Puteri'));
+// // bagaimana cara mengubah ini menjadi Arrow function?
+// ---
+// const tampilNama = (nama) => { return 'Halo, ${Puteri'};
+// console.log('Puteri Husnul');
+// // ---
+// const tampilNama = (nama, waktu) => {
+//     return `Selamat ${waktu}, ${nama}`;
+// }
+// console.log(tampilNama('hc', 'malam'));
+// ---
+// implisit return
+// const tampilNama = nama => { return `Halo, ${nama},;`}
+// console.log('Puteri husnul k');
+// ---
+// const tampilNama = () => `Hello world!`;
+// console.log(tampilNama());
+// ---
