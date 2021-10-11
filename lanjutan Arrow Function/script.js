d172a9824e0d93506a2c93a4d69c96c31a3688ad
puteri husnul k <husnulputeri@gmail.com>
2021-10-11 22:07:19 +0700

    Latihan Arrow Function

--- lanjutan Arrow Function/script.js
+++ lanjutan Arrow Function/script.js
@@ -43,25 +43,42 @@
 
 
 // // Constructor Function
-const Mahasiswa = function() {
-    this.nama = 'Puteri';
-    this.umur = 16;
-    this.sayHello = function() {
-// function ini adalah function expression disimpan ke dlm var sayHello, maka console log tidak akan kena hoisting
-        console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+// const Mahasiswa = function() {
+//     this.nama = 'Puteri';
+//     this.umur = 16;
+//     this.sayHello = function() {
+// // function ini adalah function expression disimpan ke dlm var sayHello, maka console log tidak akan kena hoisting
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//     }
+
+//     setInterval(() => {
+// // setInterval adalah function declaraton, ketika menggunakan ini akan terkena hoisting
+// // sehingga pd this dipanggil, maka akan dicek ke Global
+//     console.log(this.umur++);
+//     }, 500);
+
+// }
+
+// const puteri = new Mahasiswa();
+// // ----
+
+
+
+const box = document.querySelector('.box');
+box.addEventListener('click', function() {
+    let satu = 'size';
+    let dua = 'caption';
+
+    if (this.classList.contains(satu)) {
+        [satu, dua] = [dua, satu];
     }
 
-    setInterval(() => {
-// setInterval adalah function declaraton, ketika menggunakan ini akan terkena hoisting
-// sehingga pd this dipanggil, maka akan dicek ke Global
-    console.log(this.umur++);
-    }, 500);
-
-}
-
-const puteri = new Mahasiswa();
-// ----
-
+    this.classList.toggle(satu);
+// toggle untuk menambahkan class jikalau tidak ada, jika ada akan dihilangkan
+    setTimeout(() => {
+        this.classList.toggle(dua);
+    }, 600);
+});
 
 
 
