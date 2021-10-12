17e34d2ccacb58975fd835410fb7ea6a6cd8d8e2
puteri husnul k <husnulputeri@gmail.com>
2021-10-12 22:08:10 +0700

    Latihan filter & map

--- higher order function/script.js
+++ higher order function/script.js
@@ -2,10 +2,26 @@ const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 
 // mencari angka yg >= 3
 // for
-const newAngka = [];
-for( let i = 0; i < angka.length; i++) {
-    if( angka[i] >= 3) {
-        newAngka.push(angka[i]);
-    }
-}
+
+// const newAngka = [];
+// for( let i = 0; i < angka.length; i++) {
+//     if( angka[i] >= 3) {
+//         newAngka.push(angka[i]);
+//     }
+// }
+// console.log(newAngka);
+// --
+
+// filter
+
+// const newAngka = angka.filter(a => a >= 3);
+// console.log(newAngka);
+// --
+
+
+// Map 
+// kalikan semua angka dengan 2
+const newAngka = angka.map(a => a * 2);
 console.log(newAngka);
+
+
