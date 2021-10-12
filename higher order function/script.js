f37148de95cba4c93faf53b7b3049c30c511eb59
puteri husnul k <husnulputeri@gmail.com>
2021-10-12 22:28:07 +0700

    Latihan Filter, Map & Reduce

--- higher order function/script.js
+++ higher order function/script.js
@@ -21,7 +21,49 @@ const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 
 // Map 
 // kalikan semua angka dengan 2
-const newAngka = angka.map(a => a * 2);
-console.log(newAngka);
+
+// const newAngka = angka.map(a => a * 2);
+// console.log(newAngka);
+// --
+
+// Reduce
+// jumlahkan seluruh elemen pada array
+// reduce memiliki 2 argumen, maka harus double buka kurung
+// 0 + -1 + 8 + 9 + 1 + 4 + -5 + -4 + 3 + 2 + 9
+
+// const newAngka = angka.reduce((accumulator, currentValue) => accumulator + currentValue);
+// console.log(newAngka);
+// ---
+
+// Method Chaining
+// Cari angka > 5
+// kalikan 3
+// jumlahkan
+
+const hasil = angka.filter(a => a > 5) // 8,9,9
+.map(a => a * 3) // 24, 27, 27
+.reduce((acc, cur) => acc + cur); // 78
+console.log(hasil);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
