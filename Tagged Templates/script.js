a5ac8ed334e0845cbc994011c2acde5f3fb0e2e1
puteri husnul k <husnulputeri@gmail.com>
2021-10-17 00:20:19 +0700

    Latihan Tagged Templates

--- Tagged Templates/script.js
+++ Tagged Templates/script.js
@@ -1,21 +1,34 @@
 // Tagged Templates yaitu bentuk yg lebih kompleks dari Template Literals
 
+// const nama = 'Puteri Husnul';
+// const umur = 16;
+
+// function coba(strings, ...values) {
+// // // ...values untuk memanggil semua expression ${}
+// //     let result = '';
+// //     strings.forEach((str, i) => {
+// //         result += `${str}${values[i] || ''}`;
+// // // menggunakan || '' untuk supaya menghapus tanda undefined di console web
+// //     });
+// //     return result;
+
+// // cara gampang menggunakan higher oreder function Reduce
+// return strings.reduce((result, str, i) => `${result}${str}${values[i] || ''}
+// `, '')
+// }
+
+// const str = coba`Halo, nama saya ${nama}, dan saya ${umur} tahun.`;
+// console.log(str);
+
+
+// Highlight
 const nama = 'Puteri Husnul';
 const umur = 16;
+const email = 'husnulputeri@gmail.com';
 
-function coba(strings, ...values) {
-// // ...values untuk memanggil semua expression ${}
-//     let result = '';
-//     strings.forEach((str, i) => {
-//         result += `${str}${values[i] || ''}`;
-// // menggunakan || '' untuk supaya menghapus tanda undefined di console web
-//     });
-//     return result;
-
-// cara gampang menggunakan higher oreder function Reduce
-return strings.reduce((result, str, i) => `${result}${str}${values[i] || ''}
-`, '')
+function highlight(strings, ...values) {
+return strings.reduce((result, str, i) => `${result}${str}<span class="hl">${values[i] || ''}</span>`, '');
 }
 
-const str = coba`Halo, nama saya ${nama}, dan saya ${umur} tahun.`;
-console.log(str);
+const str = highlight`Halo, nama saya ${nama}, dan saya ${umur} tahun, dan email saya: ${email}.`;
+document.body.innerHTML = str;
