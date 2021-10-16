ec4a5386ed61ef9a4ea056d35ae6b6e2c12b19ef
puteri husnul k <husnulputeri@gmail.com>
2021-10-16 20:37:18 +0700

    Latihan Tagged Templates

--- Tagged Templates/script.js
+++ Tagged Templates/script.js
@@ -4,10 +4,18 @@ const nama = 'Puteri Husnul';
 const umur = 16;
 
 function coba(strings, ...values) {
-// ...values untuk memanggil semua expression ${}
-    return values;
+// // ...values untuk memanggil semua expression ${}
+//     let result = '';
+//     strings.forEach((str, i) => {
+//         result += `${str}${values[i] || ''}`;
+// // menggunakan || '' untuk supaya menghapus tanda undefined di console web
+//     });
+//     return result;
+
+// cara gampang menggunakan higher oreder function Reduce
+return strings.reduce((result, str, i) => `${result}${str}${values[i] || ''}
+`, '')
 }
 
 const str = coba`Halo, nama saya ${nama}, dan saya ${umur} tahun.`;
 console.log(str);
-// Not yet
