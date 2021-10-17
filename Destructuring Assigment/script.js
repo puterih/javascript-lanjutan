d74a7e0ce01bd872346d9807d9fb78b15b99af9e
puteri husnul k <husnulputeri@gmail.com>
2021-10-17 22:52:26 +0700

    Latihan Destructuring Assignments

---
+++ Destructuring Assigment/script.js
@@ -0,0 +1,14 @@
+// Destructuring Variable/ Assignment merupakas sebuah expression/sintaks pd Js yg membuat kita dpt membongkar nilai dari array atau properti dari object kedalam variabel yg terpisah/ unpacking array atau object
+
+// Destructuring Array
+// // menggunakan teknik biasa
+// const perkenalan = ['halo', 'nama', 'saya', 'Puteri'];
+// const salam = perkenalan[0];
+// const nama = perkenalan[3];
+// console.log(nama);
+
+// menggunakan Destructuring
+const perkenalan = ['halo', 'nama', 'saya', 'Puteri'];
+
+const [salam, satu, dua, nama] = perkenalan;
+console.log(dua);
