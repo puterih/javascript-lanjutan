0313af87b320943f9113b9b0bfa0af22feca8a6d
puteri husnul k <husnulputeri@gmail.com>
2021-10-18 23:18:50 +0700

    Latihan Destructuring Function

--- Destructuring Function/script.js
+++ Destructuring Function/script.js
@@ -1,14 +1,17 @@
 // Destructuring function
 
-function penjumlahanPerkalian(a, b) {
-    return [a + b, a * b];
+function kalkulasi(a, b) {
+    return [a + b, a - b, a * b, a / b];
 }
 
 // const jumlah = penjumlahanPerkalian(2, 3)[0];
 // const kali = penjumlahanPerkalian(2, 3)[1];
 // console.log(kali);
 
-const [jumlah, kali] = penjumlahanPerkalian(2, 3);
-// console.log(jumlah);
-console.log(kali);
+// const [jumlah, kali] = penjumlahanPerkalian(2, 3);
+// // console.log(jumlah);
+// console.log(kali);
+
+const [tambah, kurang, kali, bagi] = kalkulasi(2, 3);
+console.log(bagi);
 
