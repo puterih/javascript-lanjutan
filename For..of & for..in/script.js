17d71dfbb2292eced7e664413e67b94a6cb14332
puteri husnul k <husnulputeri@gmail.com>
2021-10-20 21:11:16 +0700

    Latihan for..of & for..in

---
+++ For..of & for..in/script.js
@@ -0,0 +1,64 @@
+// Looping baru pada javascript yaitu for of.. & for..in 
+// Menggunakan Array
+// const mhs = ['Puteri', 'Husnul', 'Khotimah'];
+// 1. menggunakan for biasa
+// for( let i = 0; i < mhs.length; i++ ) {
+//     console.log(mhs[i]);
+// }
+// --
+// 2. menggunakan forEach
+// mhs.forEach( m => console.log(m));
+// --
+// 3. mengguanakan for..of
+for( const m of mhs ) {
+    console.log(m);
+}
+// --
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
