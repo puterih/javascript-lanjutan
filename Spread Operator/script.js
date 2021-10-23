3eea743c713cc378fe6260129bc28d63f11ed49a
puteri husnul k <husnulputeri@gmail.com>
2021-10-23 23:29:24 +0700

    Latihan Spread Operator

--- Spread Operator/script.js
+++ Spread Operator/script.js
@@ -10,10 +10,54 @@
 
 
 // menggabungkan 2 array, atau lebih
+// const mhs = ['Puteri', 'Husnul', 'Khotimah'];
+// const dosen = ['hana', 'erza', 'veza'];
+// // // cara menggabungkannya
+// const orang = [...mhs, 'aji', ...dosen];
+// // bisa menggunakan cara yang lain menggunakan sebuah method array yaitu concat
+// // const orang = mhs.concat(dosen);
+// console.log(orang);
+
+
+
+// meng-copy array
 const mhs = ['Puteri', 'Husnul', 'Khotimah'];
-const dosen = ['hana', 'erza', 'veza'];
-// // cara menggabungkannya
-const orang = [...mhs, 'aji', ...dosen];
-// bisa menggunakan cara yang lain menggunakan sebuah method array yaitu concat
-// const orang = mhs.concat(dosen);
-console.log(orang);
+// const mhs1 = mhs;
+const mhs1 = [...mhs];
+mhs1[0] ='Hace';
+// console.log(mhs);
+console.log(mhs1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
