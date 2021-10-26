af86dbbb8bb881438e83f803033e9efa90371447
puteri husnul k <husnulputeri@gmail.com>
2021-10-27 06:59:24 +0700

    Latihan Rest Parameter

--- Spread Operator & Rest Parameter/script2.js
+++ Spread Operator & Rest Parameter/script2.js
@@ -20,16 +20,31 @@
 
 
 // Cara 3
-function myFunc() {
-    // // ...myArgs akan membentuk array
-    //     return arguments;
-    // di console akan menghasilkan argument bentuknya object, maka harus diubah menjadi array dahulu
-        return Array.from(arguments);
-    // // atau menggunakan spread operator
-    //     return [...arguments];
-    }
+// function myFunc() {
+//     // // ...myArgs akan membentuk array
+//     //     return arguments;
+//     // di console akan menghasilkan argument bentuknya object, maka harus diubah menjadi array dahulu
+//         return Array.from(arguments);
+//     // // atau menggunakan spread operator
+//     //     return [...arguments];
+//     }
     
-console.log(myFunc(1,2,3,4,5));
+// console.log(myFunc(1,2,3,4,5));
+
+
+// Cara 1: Penjumlahan parameter menggunakan for..of
+function jumlahkan(...angka) {
+    let total = 0;
+// menggunakan let karna akan akan dimasukkan ke dalam Looping
+    for(const a of angka) {
+        total += a;
+    }
+
+    return total;
+
+}
+
+console.log(jumlahkan(1,2,3,4,5));
 
 
 
