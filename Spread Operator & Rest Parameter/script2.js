1955cd63f4e6564625dbce018ab1aecd1edbd699
puteri husnul k <husnulputeri@gmail.com>
2021-10-27 06:50:39 +0700

    Latihan Rest Parameter

--- Spread Operator & Rest Parameter/script2.js
+++ Spread Operator & Rest Parameter/script2.js
@@ -10,10 +10,51 @@
 
 
 // cara 2
-function myFunc(...myArgs) {
-    // ...myArgs akan membentuk array
-        return myArgs;
+// function myFunc(...myArgs) {
+//     // ...myArgs akan membentuk array
+//         return myArgs;
+//     }
+    
+// console.log(myFunc(1,2,3,4,5));
+// // Rest(sisa)...myArgs parameter hanya bisa digunakan di akhir dari argument
+
+
+// Cara 3
+function myFunc() {
+    // // ...myArgs akan membentuk array
+    //     return arguments;
+    // di console akan menghasilkan argument bentuknya object, maka harus diubah menjadi array dahulu
+        return Array.from(arguments);
+    // // atau menggunakan spread operator
+    //     return [...arguments];
     }
     
 console.log(myFunc(1,2,3,4,5));
-// Rest(sisa)...myArgs parameter hanya bisa digunakan di akhir dari argument
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
